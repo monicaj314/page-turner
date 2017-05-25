1eb04cb90fb5b5fe986fde26a9fd62937f24335e
Victor M Merino <vmerino04@gmail.com>
2017-05-25 13:10:04 -0500

    adds stickyness to left nav

--- src/components/LeftNav/LeftNav.js
+++ src/components/LeftNav/LeftNav.js
@@ -6,7 +6,8 @@ class LeftNav extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      expandedMenu:'NYT'
+      expandedMenu:'NYT',
+      sticky: false
     }
   }
 
@@ -16,32 +17,48 @@ class LeftNav extends React.Component {
     })
   }
 
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
+
+  onScroll = () => {
+    if (window.scrollY >= 96 && !this.state.sticky) {
+      this.setState({sticky: true})
+    } else if (window.scrollY < 96 && this.state.sticky) {
+      this.setState({sticky: false});
+    }
+  }
+
   getCategories(menuCode){
     return this.props.categories.filter(cat => cat.listSourceId === menuCode && cat.visible)
   }
 
   render(){
+
     if (this.props.isFetchingCategories){
       return <p>Loading...</p>
     } else {
       return (
-        <div className='left-nav'>
-          <div className='nav-body'>
-            <CategoryMenu menuName='New York Times'
-              menuCode='NYT'
-              handleExpanding={this.handleExpanding }
-              handleCategoryChange={this.props.handleCategoryChange}
-              selectedCategoryId={this.props.selectedCategoryId}
-              categories={this.getCategories('NYT')}
-              expanded={this.state.expandedMenu === 'NYT'}/>
-            <CategoryMenu menuName='Amazon'
-              menuCode='AMZ'
-              handleExpanding={this.handleExpanding}
-              handleCategoryChange={this.props.handleCategoryChange}
-              selectedCategoryId={this.props.selectedCategoryId}
-              categories={this.getCategories('AMZ')}
-              expanded={this.state.expandedMenu === 'AMZ'}/>
-
+        <div className='left-nav-wrapper'>
+          <div className={`left-nav ${this.state.sticky ? 'sticky':''}`}>
+              <CategoryMenu menuName='New York Times'
+                menuCode='NYT'
+                handleExpanding={this.handleExpanding }
+                handleCategoryChange={this.props.handleCategoryChange}
+                selectedCategoryId={this.props.selectedCategoryId}
+                categories={this.getCategories('NYT')}
+                expanded={this.state.expandedMenu === 'NYT'}/>
+              <CategoryMenu menuName='Amazon'
+                menuCode='AMZ'
+                handleExpanding={this.handleExpanding}
+                handleCategoryChange={this.props.handleCategoryChange}
+                selectedCategoryId={this.props.selectedCategoryId}
+                categories={this.getCategories('AMZ')}
+                expanded={this.state.expandedMenu === 'AMZ'}/>
           </div>
         </div>
       )
