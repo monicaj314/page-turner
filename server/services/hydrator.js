92ca10052fe160ef2c456b9d2674f53906ad8ddf
Victor M Merino <vmerino04@gmail.com>
2017-06-01 19:33:56 -0500

    updates hydrator

--- server/services/hydrator.js
+++ server/services/hydrator.js
@@ -3,9 +3,25 @@ const cache = require('../lib/cache')
 const twelveHours = 12*60*60*1000
 const twentyFourHours = 24*60*60*1000
 
-setInterval(() => {
+const input = process.argv[2]
+
+if (input === '-b') {
+  hydrateBooks()
+} else if (input === '-c') {
+  hydrateCategories()
+} else if (input === '-i') {
+  console.log(`Hydrating and setting up interval for: Categories(${twentyFourHours} s), Books(${twelveHours} s)`)
+  console.log(`------------------------------------------------------------------------------`)
+  hydrateCategories().then(hydrateBooks)
+  setInterval(hydrateCategories, twentyFourHours)
+  setInterval(hydrateBooks, twelveHours)
+} else {
+  hydrateCategories().then(hydrateBooks)
+}
+
+function hydrateCategories(){
   console.log(`Hydrating Categories (Time: ${new Date()})`)
-  PageTurners.fetchCategories()
+  return PageTurners.fetchCategories()
     .then(data => {
       const key = 'categories'
       cache.setCategories(key, JSON.stringify(data))
@@ -14,27 +30,32 @@ setInterval(() => {
     .catch(err => {
       console.error(err)
     })
-}, twentyFourHours)
+}
 
-setInterval(() => {
+function hydrateBooks(){
   console.log(`Hydrating Best Sellers (Time: ${new Date()})`)
-  PageTurners.getCategories()
-    .then(data => {
-      const categories = data.filter(cat => cat.visible)
-      for (let i = 0; i < categories.length; i++){
-        setTimeout(() => {
-          const category = categories[i]
-          console.log(`Time: ${new Date()} - Starting '${category.name}'.`)
-          PageTurners.fetchBestSellers(category)
-            .then(data => {
-              const key = category.id
-              cache.setBestSellers(key, JSON.stringify(data))
-              console.log(`------------------------------------------------------------------------------`)
-            })
-            .catch(err => {
-              console.error(err)
-            })``
-        }, i * 30000)
-      }
-    })
-}, twelveHours)
+  return PageTurners.getCategories()
+    .then(allCategories => hydrateBestSellersForCategories(allCategories.filter(cat => cat.visible)))
+}
+
+function hydrateBestSellersForCategories(categories) {
+  for (let i = 0; i < categories.length; i++){
+    setTimeout(() => {
+      const category = categories[i]
+      console.log(`Time: ${new Date()} - Starting '${category.name}'.`)
+      hydrateSingleCategory(category)
+    }, i * 10000)
+  }
+}
+
+function hydrateSingleCategory(category){
+  PageTurners.fetchBestSellers(category)
+  .then(data => {
+    const key = category.id
+    cache.setBestSellers(key, JSON.stringify(data))
+    console.log(`------------------------------------------------------------------------------`)
+  })
+  .catch(err => {
+    console.error(`Hydrator error:${err}`)
+  })
+}
